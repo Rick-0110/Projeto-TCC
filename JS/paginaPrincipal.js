b1e8c3be58ab1bd67b01db883167a039e5c7dda4
Henrique <matosh1604@gmail.com>
2025-04-24 17:42:24 -0300

    Atualizações na área de perfil

--- JS/paginaPrincipal.js
+++ JS/paginaPrincipal.js
@@ -7,7 +7,11 @@ window.location.href = 'carrinho.html';
 
 });
 
-function servicos(){
-window.location.href = "servicos.html";
-}
+document.querySelector('.fa-wrench').parentElement.addEventListener('click', function(){
+    window.location.href = 'servicos.html';
+});
+
+document.querySelector('.fa-user').parentElement.addEventListener('click', function(){
+    window.location.href = 'perfil.html';
+});
     
